fb09fdf550581a077e4ff86427e4ba569c76a735
Denis-pyth <desirefeyisola2@gmail.com>
2026-02-11 18:12:15 -0800

    feat(docs): add Swagger UI and OpenAPI documentation for noahs ark shop API

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { initRedis } from './config/redis.js';
 import authRoutes from "./routes/auth.route.js";
 import ordersRoutes from "./routes/orders.route.js";
-import productRoutes from "./routes/product.route.js"
-import { authJwt } from './middleware/authJwt.js';
+import productRoutes from "./routes/product.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import { jwtAuth } from './middleware/authJwt.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import dotenv from 'dotenv';
 import { initializeDB } from './db/db.js';
+import { validateSession } from './middleware/session.js';
+import swaggerUi from 'swagger-ui-express';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import YAML from 'yamljs';
 
 const app = express();
 dotenv.config();
@@ -13,14 +21,31 @@ dotenv.config();
 initializeDB();
 
 const PORT = process.env.PORT || 3000;
-
+await initRedis(); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(authJwt());
+
+const swaggerPath = path.join(__dirname, 'docs', 'swagger.yaml');
+console.log('Swagger file path:', swaggerPath);
+
+if (!fs.existsSync(swaggerPath)) {
+    console.error(' Swagger file not found at:', swaggerPath);
+} else {
+    console.log(' Swagger file found at:', swaggerPath);
+}
+
+const swaggerDocument = YAML.load(swaggerPath);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(jwtAuth);
+app.use(validateSession);
 app.use('/api/auth', authRoutes);
 app.use("/api/orders", ordersRoutes);
 app.use("/api/products", productRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
